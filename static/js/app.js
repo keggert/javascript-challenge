c167d45d40b5e43551637252d4fca3cd06eabecf
Kevin Eggert <kevineggert@Kevins-MacBook-Air.local>
2020-08-26 23:52:49 -0500

    Watched the solution video but code refuses to work

--- static/js/app.js
+++ static/js/app.js
@@ -3,8 +3,9 @@ var tableData = data;
 
 // YOUR CODE HERE!
 // get table references
-var table = d3.select("table");
 var tbody = d3.select("tbody");
+var tableData = data;
+console.log("tbody");
 
 function buildTable(data) {
     // Clear out existing data
@@ -12,34 +13,34 @@ function buildTable(data) {
     // Loop through each object in the data and append a row and cells for each value in the row
     data.forEach((dataRow) => {
         // Append the row
-        const row = tbody.append("tr");
-
+        let row = tbody.append("tr");
         // Loop through the field in the data row and add each value as a table cell (td)
-        Object.values(dataRow).forEach((value) => {
+        Object.values(dataRow).forEach((val) => {
             let cell = row.append("td");
-                cell.text(value);
+            cell.text(val);
             }
         );
     });
-}
+};
 
-function handleClick() {
+function handleClic() {
+    d3.event.preventDefault();
     // Grab the datetime value from the filter
-    const date = d3.select("#datetime").property("value");
+    let date = d3.select("#datetime").property("value");
     let filterData = tableData;
 
     // See if date was entered and filter data uusing that particular date
     if (date) {
         // Apply filter to table data to only keep rows where 'datetime' value matches the filter value
-        filterData = filterData.filter(row => row.dateTime === date);
-    }
+        filterData = filterData.filter((row) => row.dateTime === date);
+    };
     
     // Rebuild table using filtered data
     buildTable(filterData);
-}
+};
 
 // Attach an event to listen for form button
-d3.selectAll("#filter-btn").on("click", handleClick);
+d3.selectAll("#filter-btn").on("click", handleClic);
 
 //Build the table when page loads
-buildTable(tableData)
+buildTable(tableData);
